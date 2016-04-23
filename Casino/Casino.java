a07df6d9d70a9e90ecb7a06c2a62d7062990fb59
tailcalled <tailcalled@gmail.com>
2016-04-23 10:31:56 +0200

    Solved Casino (...ish, performance might be bad)

--- Casino/Casino.java
+++ Casino/Casino.java
@@ -3,6 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 /*
@@ -12,6 +16,7 @@ Testdata:
 14 13 12 11 10 8 7 4
 
 Might yield:
+(not really - the example is broken according to the host)
 
 13
 12
@@ -35,32 +40,47 @@ Might yield:
  */
 
 public class Casino {
-
+	
 	static ArrayList<Integer> hand, table;
-	static ArrayList<ArrayList<Integer>> combos;
+	static ArrayList<Integer> cards;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader lineIn = new BufferedReader(new InputStreamReader(System.in));
-		hand = new ArrayList<>();
-		readCards(lineIn, hand);
-		table = new ArrayList<>();
-		readCards(lineIn, table);
-		combos = new ArrayList<>();
-		
+		hand = readCards(lineIn);
+		table = readCards(lineIn);
+		cards = new ArrayList<>(hand); cards.addAll(table);
+		for (int i = 0; i < hand.size(); i++) {
+			solve(0, i, hand.get(i));
+		}
 	}
+	
+	static ArrayList<Integer> chosen = new ArrayList<>();
 
-	private static void readCards(BufferedReader lineIn, ArrayList<Integer> list) throws IOException {
-		StringTokenizer in = new StringTokenizer(lineIn.readLine());
-		while (in.hasMoreTokens()) {
-			String tok = in.nextToken();
-			switch (tok) {
-			case "A": list.add(null); break;
-			case "K": list.add(13); break;
-			case "Q": list.add(12); break;
-			case "J": list.add(11); break;
-			default: list.add(Integer.parseInt(tok)); break;
+	private static void solve(int current, int skip, int target) {
+		if (target == 0) {
+			for (int i: chosen) System.out.print(i + " ");
+			System.out.println();
+			return;
+		}
+		else if (current < cards.size()) {
+			solve(current + 1, skip, target);
+			if (current != skip && target >= cards.get(current)) {
+				chosen.add(cards.get(current));
+				solve(current + 1, skip, target - cards.get(current));
+				chosen.remove(chosen.size() - 1);
 			}
 		}
+	}
+
+	private static ArrayList<Integer> readCards(BufferedReader lineIn) throws IOException {
+		Scanner in = new Scanner(lineIn.readLine());
+		ArrayList<Integer> list = new ArrayList<>();
+		while (in.hasNext()) {
+			int i = in.nextInt();
+			if (i == 14) i = 1;
+			list.add(i);
+		}
+		return list;
 	}
 
 }
